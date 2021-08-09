c15e55b2f647ee78ce861bd8b9e5753fda1618e8
Svetlana Mikisheva <svetamikis@yandex.ru>
2021-08-10 00:39:02 +0500

    добавлен код в файл task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+ result = arr1.every((item, idx) => {
+      if(arr1.length === arr2.length) {
+        return item === arr2[idx];
+      } 
+  })
 
   return result; // boolean
 }
@@ -9,7 +13,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
