b1da81fad69c5cce3b7ee50c2e164913fb4dc734
Svetlana Mikisheva <svetamikis@yandex.ru>
2021-08-17 20:59:26 +0500

    добавлен код для решения задачи

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+	this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+
+	if (this.marks === undefined) {
+      this.marks = [];
+      this.marks.push(mark);
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+	this.marks =  Object.values(mark);
+}
+
+Student.prototype.getAverage = function () {
+	let sum = 0;
+  	let avg = 0;
+
+  	for(let i = 0; i < this.marks.length; i++) {
+  		sum += this.marks[i];
+  		avg = Number(sum / this.marks.length);
+  }
+
+  this.avg = avg;
+  
+  return this.avg;
+}
+
+Student.prototype.exclude = function (reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
 
 // ваш код для остальных методов
