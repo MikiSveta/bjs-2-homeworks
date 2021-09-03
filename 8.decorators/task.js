e097c3ad5eec18f409268f68096c6322fd46d957
Svetlana Mikisheva <svetamikis@yandex.ru>
2021-09-04 00:14:39 +0500

    добавлен код для задач 1-3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,56 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+
+function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+    let idx = cache.findIndex((item)=> hash === item.hash); // ищем элемент, хэш которого равен нашему хэшу
+    
+    if (idx !== -1 ) { // если элемент не найден
+        console.log("Из кэша: " + cache[idx].value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+        return "Из кэша: " + cache[idx].value;
+    } else {
+        let result = func(...args); // в кэше результата нет - придётся считать
+        cache.push({hash, value: result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+          cache.shift() ; // если слишком много элементов в кэше надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+}
+return wrapper;
+  
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+
+	let timeout = false;
+
+    function wrapper(...args) {
+        if (timeout) return;
+        func.apply(this, args);
+        timeout = true;
+        setTimeout(() => timeout = false, ms);
+    };
+
+    return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+
+    let timeout = false;
+
+    function wrapper(...args) {
+
+        wrapper.count.push(args);
+        return func.call(this, ...args);
+        if (timeout) return;
+        func.apply(this, args);
+        timeout = true;
+        setTimeout(() => timeout = false, ms);
+    };
+
+        wrapper.count = [];
+        return wrapper;
 }
